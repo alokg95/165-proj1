6184173907f1cc3cf6f3dabc332b50d03d4ff146
Alok Gupta <alok.gupta@shiphawk.com>
2016-04-22 16:49:10 -0700

    cleaned

--- triclassify.py
+++ triclassify.py
@@ -21,9 +21,6 @@ with open(sys.argv[1], 'r') as f:
     feature_b_avg = feature_b_total/float(a_size)
     feature_c_avg = feature_c_total/float(a_size)
     class_a_avg = [feature_a_avg, feature_b_avg, feature_c_avg]
-    print "-----------------------------------------------------"
-    print "Class A average:",class_a_avg
-
 
     feature_a_total = 0
     feature_b_total = 0
@@ -38,8 +35,6 @@ with open(sys.argv[1], 'r') as f:
     feature_b_avg = feature_b_total/float(b_size)
     feature_c_avg = feature_c_total/float(b_size)
     class_b_avg = [feature_a_avg, feature_b_avg, feature_c_avg]
-    print "-----------------------------------------------------"
-    print "Class B average:",class_b_avg
 
     feature_a_total = 0
     feature_b_total = 0
@@ -54,9 +49,6 @@ with open(sys.argv[1], 'r') as f:
     feature_b_avg = feature_b_total/float(c_size)
     feature_c_avg = feature_c_total/float(c_size)
     class_c_avg = [feature_a_avg, feature_b_avg, feature_c_avg]
-    print "-----------------------------------------------------"
-    print "Class C average:",class_c_avg
-    print "-----------------------------------------------------"
 
     vector_ab = [class_b_avg[0] - class_a_avg[0], class_b_avg[1] - class_a_avg[1], class_b_avg[2] - class_a_avg[2]]
     # print "vector a->b:", vector_ab
@@ -116,7 +108,6 @@ with open(sys.argv[2], 'r') as f:
     for i in range(int(line_one[1])):
         estimated = "A"
         line = f.readline().split()
-        print i, line, "A"
         diff_a = [class_a_avg[0]-float(line[0]), class_a_avg[1]-float(line[1]),class_a_avg[2]-float(line[2])]
         diff_a = np.linalg.norm(diff_a)
 
@@ -131,19 +122,16 @@ with open(sys.argv[2], 'r') as f:
         # print differences
         # print result
         if(result == diff_a):
-            print 'Given Class A data, Predicted: A - TRUE'
             correct_guess += 1
             a_stats[0] += 1
             b_stats[1] += 1
             c_stats[1] += 1
         elif(result == diff_b):
-            print 'Predicted: B - FALSE'
             incorrect_guess += 1
             a_stats[3] += 1
             b_stats[2] += 1
             c_stats[1] += 1
         elif(result == diff_c):
-            print 'Predicted: C - FALSE'
             incorrect_guess += 1
             a_stats[3] += 1
             b_stats[1] += 1
@@ -154,7 +142,6 @@ with open(sys.argv[2], 'r') as f:
     for i in range(int(line_one[1]), second_range):
         estimated = "B"
         line = f.readline().split()
-        print i, line
         diff_a = [class_a_avg[0]-float(line[0]), class_a_avg[1]-float(line[1]),class_a_avg[2]-float(line[2])]
         diff_a = np.linalg.norm(diff_a)
 
@@ -169,19 +156,16 @@ with open(sys.argv[2], 'r') as f:
         # print differences
         # print result
         if(result == diff_a):
-            print 'Given Class B data, Predicted: A - FALSE'
             incorrect_guess += 1
             a_stats[2] += 1
             b_stats[3] += 1
             c_stats[1] += 1
         elif(result == diff_b):
-            print 'Given Class B data, Predicted: B - TRUE'
             correct_guess += 1
             a_stats[1] += 1
             b_stats[0] += 1
             c_stats[1] += 1
         elif(result == diff_c):
-            print 'Given Class B data, Predicted: C - FALSE'
             incorrect_guess += 1
             a_stats[1] += 1
             b_stats[3] += 1
@@ -190,7 +174,6 @@ with open(sys.argv[2], 'r') as f:
     for i in range(second_range, 75):
         estimated = "C"
         line = f.readline().split()
-        print i, line
         diff_a = [class_a_avg[0]-float(line[0]), class_a_avg[1]-float(line[1]),class_a_avg[2]-float(line[2])]
         diff_a = np.linalg.norm(diff_a)
 
@@ -205,36 +188,22 @@ with open(sys.argv[2], 'r') as f:
         # print differences
         # print result
         if(result == diff_a):
-            print 'Given Class C data, Predicted: A - FALSE'
             incorrect_guess += 1
             a_stats[2] += 1
             b_stats[1] += 1
             c_stats[3] += 1
         elif(result == diff_b):
-            print 'Given Class C data, Predicted: B - FALSE'
             incorrect_guess += 1
             a_stats[1] += 1
             b_stats[2] += 1
             c_stats[3] += 1
         elif(result == diff_c):
-            print 'Given Class C data, Predicted: C - TRUE'
             correct_guess += 1
             a_stats[1] += 1
             b_stats[1] += 1
             c_stats[0] += 1
 
         total = i + 1
-
-print "--------------------------------------------------------------------"
-print "Summary"
-print "--------------------------------------------------------------------"
-print "Total Guesses:", total
-print "Correct Guesses:", correct_guess
-print "Incorrect Guesses:", incorrect_guess
-
-print a_stats
-print b_stats
-print c_stats
 
 TPR_a = a_stats[0]/float(line_one[1])
 TPR_b = b_stats[0]/float(line_one[1])
@@ -253,12 +222,18 @@ precision_a = a_stats[0]/float(a_stats[0]+a_stats[2])
 precision_b = b_stats[0]/float(b_stats[0]+b_stats[2])
 precision_c = c_stats[0]/float(c_stats[0]+c_stats[2])
 
+accuracy_a = float(a_stats[0]+a_stats[1])/float(float(line_one[1])+float(line_one[2])+float(line_one[3]))
+accuracy_b = float(b_stats[0]+b_stats[1])/float(float(line_one[1])+float(line_one[2])+float(line_one[3]))
+accuracy_c = float(c_stats[0]+c_stats[1])/float(float(line_one[1])+float(line_one[2])+float(line_one[3]))
+
 TPR_avg = (TPR_a+TPR_b+TPR_c)/float(line_one[0])
 FPR_avg = (FPR_a+FPR_b+FPR_c)/float(line_one[0])
 error_avg = (error_a+error_b+error_c)/float(line_one[0])
+accuracy_avg = (accuracy_a+accuracy_b+accuracy_c)/3
 precision_avg = (precision_a+precision_b+precision_c)/3
 
-print "TPR AVG:", TPR_avg
-print "FPR_avg:", FPR_avg
-print "Error Avg", error_avg
-print "Precision avg", precision_avg
+print "True positive rate =", TPR_avg
+print "False positive rate =", FPR_avg
+print "Error Rate =", error_avg
+print "Accuracy =", accuracy_avg
+print "Precision =", precision_avg
